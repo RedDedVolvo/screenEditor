91ccb74f67c99cced365f3751b11318db54d7739
Nick Farland <nfarland1990@gmail.com>
2020-11-12 11:55:24 -0600

    First commit - version 0.01 file structure and files

---
+++ src/server/server.js
@@ -0,0 +1,5 @@
+// This file serves to take the final image product and send it to the designated directory to save
+// Its secondary function is to operate as the in-between for the Chrome extension and the service
+
+const fs = require('fs');
+const os = require('os');
